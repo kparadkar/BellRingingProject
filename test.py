6bf9545bfa4a164d88d000b0a09d0c83b8c97054
Sarjeet Singh <sarjeetsingh@maprtech.com>
2017-02-28 13:05:29 -0800

    New Email template in test.py

--- test.py
+++ test.py
@@ -6,10 +6,27 @@ import base64
 from email.mime.text import MIMEText
 import os
 import json
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+import datetime
 
-URL = "http://10.10.104.46:8082"
+URL = "http://54.219.187.57:8082"
 LOCALHOST = "127.0.0.1"
 TOPIC = "notify"
+
+# method to get greet message
+def get_greeting_msg():
+
+    currentTime = datetime.datetime.now()
+    print "Current hour: ", currentTime.hour
+
+    if currentTime.hour < 12:
+        return 'Good Morning,'
+    elif 12 <= currentTime.hour < 18:
+        return 'Good Afternoon,'
+    else:
+        return 'Good Evening,'
 
 # Email method for sending email
 def send_email(body):
@@ -21,25 +38,75 @@ def send_email(body):
     deals = []
     size = 0
     for item in body_json:
-        costumers.append (base64.b64decode((item["key"])))
-        deals.append (base64.b64decode((item["value"])))
+        customer = base64.b64decode((item["key"]))
+        if customer.startswith('"') and customer.endswith('"'):
+            customer = customer[1:-1]
+        costumers.append(customer)
+
+        deal = base64.b64decode((item["value"]))
+        if deal.startswith('"') and deal.endswith('"'):
+            deal = deal[1:-1]
+        deals.append(deal)
         size += 1
     
     text = ""
     for i in range(0, size):
-        text = text + "Customer name: " + costumers[i] + "\n"
-        text = text + "Deal size: " + deals[i] + "\n"
+        text = text + "Customer name: " + costumers[i] + "<\n>"
+        text = text + "Deal size: " + deals[i] + "<\n>"
 
-    print text
-
-    text_msg = MIMEText(text, 'plain')
+    # Above text in table format
+    table_text = ""
+    for i in range(0, size):
+        table_text += "<tr>"
+        table_text += "<td width='50%'>" + costumers[i] + "</td>"
+        table_text += "<td width='50%'>" + deals[i] + "</td>"
+        table_text += "</tr>"
 
     me = "maprbell2017@gmail.com"
     you = "sarjeetsingh@maprtech.com"
 
-    text_msg['Subject'] = 'Bell Project deal notification'
-    text_msg['From'] = me
-    text_msg['To'] = you
+    msg = MIMEMultipart('related')
+    msg['Subject'] = 'Bell Project deal notification'
+    msg['From'] = me
+    msg['To'] = you
+
+    # Encapsulate the plain and HTML versions of the message body in an
+    # 'alternative' part, so message agents can decide which they want to display.
+    msgAlternative = MIMEMultipart('alternative')
+    msg.attach(msgAlternative)
+
+    msgText = MIMEText(text)
+    msgAlternative.attach(msgText)
+
+
+    ##### Image text
+    greetMsg = get_greeting_msg()
+    image_txt = \
+    '''
+    <b>''' + greetMsg + ''' </b><br><br><img src="cid:image1"><br><br><br>
+    <b> Here is the latest customer deal: </b><br><br>
+    <table border="1" style="width:80%">
+    <tr>
+    <th>Customer name</th>
+    <th>Deal Size</th>
+    </tr>
+    <p> ''' + table_text  + ''' </p>
+    </table><br>
+    <p> Thanks, <br>Bell-project team!<br>
+    '''
+
+    # We reference the image in the IMG SRC attribute by the ID we give it below
+    msgText = MIMEText(image_txt, 'html')
+    msgAlternative.attach(msgText)
+
+    # Assume the image is in the current directory
+    fp = open('bell.png', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+
+    # Define the image's ID as referenced above
+    msgImage.add_header('Content-ID', 'image1')
+    msg.attach(msgImage)
 
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
@@ -53,7 +120,7 @@ def send_email(body):
     s.login(USER, PASSWD)
     #s.set_debuglevel(True)
     try:
-        s.sendmail(me, [you], text_msg.as_string())
+        s.sendmail(me, [you], msg.as_string())
     finally:
         s.quit()
 
@@ -71,9 +138,9 @@ CONSUMER_DELETE_URL = URL + "/consumers/" + GROUP + "/instances/" + CONSUMER
 CONSUMER_CONSUME_URL = URL + "/consumers/" + GROUP + "/instances/" + CONSUMER + "/topics/" + TOPIC
 
 # Delete consumer instance if exists already
-#h = httplib2.Http()
-#resp, content = h.request(CONSUMER_DELETE_URL, method="DELETE")
-#print resp.status, content
+h = httplib2.Http()
+resp, content = h.request(CONSUMER_DELETE_URL, method="DELETE")
+print resp.status, content
 
 # Create new instance of consumer
 CONSUMER_POST_DATA = {}
