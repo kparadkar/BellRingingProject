5ddadf575ccc40d60778ec637446e3ecbbc7cbd8
Kimaya Paradkar <kparadkar@maprtech.com>
2017-03-01 20:07:27 -0800

    Update resetSetup.sh
    Set topic permissions on a stream

--- resetSetup.sh
+++ resetSetup.sh
@@ -1,5 +1,5 @@
 maprcli stream delete -path /bell-project
 maprcli stream create -path /bell-project
-maprcli stream edit -path /bell-project -produceperm p -consumeperm p -copyperm p
+maprcli stream edit -path /bell-project -produceperm p -consumeperm p -copyperm p -topicperm p
 maprcli stream topic create -path /bell-project -topic notify
 maprcli stream topic create -path /bell-project -topic bellNotify
